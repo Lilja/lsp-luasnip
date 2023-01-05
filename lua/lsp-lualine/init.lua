dbdede6fc987463ce8c63ef562e94f80e3d88510
Erik Lilja <erik.lilja@funnel.io>
2023-01-05 22:18:20 +0100

    work

--- lua/lsp-lualine/init.lua
+++ lua/lsp-lualine/init.lua
@@ -1,24 +1,17 @@
-
 local ls = require("luasnip")
-
-local snippet = ls.snippet
-local text = ls.text_node
-local func = ls.function_node
-local insert = ls.insert_node
-
 
 local M = {}
 
-function M.setup()
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    -- local bufnr = args.buf
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-		local root_dir = client["config"].root_dir
-		local file_types = client["config"].filetypes
-		local lsp_name = client.name
-		M.registerSnippet(root_dir, file_types, lsp_name)
-		-- print(vim.inspect(filetypes))
+function M.setup(opts)
+  opts = opts or {}
+
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      local root_dir = client["config"].root_dir
+      local file_types = client["config"].filetypes
+      local lsp_name = client.name
+      M.registerSnippet(root_dir, file_types, lsp_name, opts.snippets)
   end,
 })
 end
@@ -37,107 +30,37 @@ function parserPackageJson(workspace)
   return output
 end
 
-function M.registerSnippet(root_dir, filetypes, lsp_name)
-  if lsp_name == "tsserver" then
-    local snippets = {}
+function M.registerSnippet(root_dir, filetypes, lsp_name, global_snippets)
+  local output = {}
+  if lsp_name == "tsserver" or lsp_name == "volar" then
     local package_json = parserPackageJson(root_dir)
-    for _, v in pairs(M.typescriptSnippets()) do
-      local out = M.nodeJsShouldAddSnippet(v, package_json)
-      if out ~= nil then
-        table.insert(snippets, out)
+    for _, filetype in pairs(filetypes) do
+      for _, v in pairs(global_snippets) do
+        local out = M.nodeJsShouldAddSnippet(v, package_json)
+        if out ~= nil then
+          if output[filetype] ~= nil then
+            output[filetype] = {}
+          end
+          table.insert(output[filetype], out)
+        end
       end
     end
-    ls.add_snippets(nil, {
-      typescript = snippets,
-    })
-  end
-  if lsp_name == "volar" then
-    local snippets = {}
-    local package_json = parserPackageJson(root_dir)
-    for _, v in pairs(M.vue()) do
-      local out = M.nodeJsShouldAddSnippet(v, package_json)
-      if out ~= nil then
-        table.insert(snippets, out)
-      end
-    end
-    ls.add_snippets(nil, {
-      vue = snippets,
-    })
+    ls.add_snippets(nil, output)
   end
 end
 
-function M.nodeJsShouldAddSnippet(decoratedSnip, package_json)
-  local depName = decoratedSnip[1]
-  local depOrDevDep = decoratedSnip[2]
-  local acceptedValue = decoratedSnip[3]
+function M.nodeJsShouldAddSnippet(decoratedSnip, package_json, lsp_filetype)
+  local filetype = decoratedSnip[1]
+  local depName = decoratedSnip[2]
+  local depOrDevDep = decoratedSnip[3]
+  local acceptedValue = decoratedSnip[4]
   local x = package_json[depOrDevDep][depName]
-  if x then
+  if x and filetype == lsp_filetype then
     if acceptedValue == "present" then
-      return decoratedSnip[4]
+      return decoratedSnip[5]
     end
   end
   return nil
-end
-
-function M.typescript()
-	local fastifySnippet = {
-    "fastify",
-    "dependencies",
-    "present",
-		snippet({
-			trig = "fastifyScope",
-		}, {
-			text({"import fastify, {",
-			"  FastifyError,",
-			"  FastifyInstance,",
-			"  FastifyPluginOptions,",
-			"} from \"fastify\";",
-			"",
-			"const "
-			}),
-			insert(1, "routes"),
-			text({" = (",
-			"  fastify: FastifyInstance,",
-			"  options: FastifyPluginOptions,",
-			"  next: (error?: FastifyError) => void",
-			") => {",
-			"  // Powered by luasnip + custom func",
-			"  next();",
-			"};"})
-		})
-	}
-  return {fastifySnippet}
-end
-
-function M.vue()
-  local vueTemplate = {
-    "vue",
-    "dependencies",
-    "present",
-		snippet({
-			trig = "vue 3 script+template",
-      dscr = "Vue 3 script(setup)+template",
-		}, {
-			text({"<script setup lang=\"ts\">",
-      "import {ref, computed, PropType } from \"vue\"",
-			"",
-			"const props = defineProps({",
-      "  type: {",
-      "    required: true,",
-      "    type: String as PropType<string>,",
-      "  }",
-      "})",
-			"</script>",
-			"<template>",
-      "  <div></div>",
-			"</template>",
-      })
-    })
-  }
-
-  return {
-    vueTemplate
-  }
 end
 
 return M
